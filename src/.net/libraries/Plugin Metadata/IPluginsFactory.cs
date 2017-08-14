402a6aea8d107b6bcea2e550b786e62d5a359470
Arvydas Grigonis <arvydas@lythum.lt>
2017-08-14 13:45:22 +0300

    Basic Backbone

---
+++ src/.net/libraries/Plugin Metadata/IPluginsFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedTools.PluginMetadata
+{
+	/// <summary>
+	/// Main interface which shall be received from plygin DLL file, 
+	/// which enumerates all availe in specific DLL file plugins
+	/// </summary>
+	public interface IPluginsFactory
+	{
+		IEnumerable<IPlugin> Plugins { get; }
+	}
+}
